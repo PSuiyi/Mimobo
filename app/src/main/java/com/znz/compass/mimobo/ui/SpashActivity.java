3a510ea4a66a98b8b3ab396784849954883244d4
panqiming <497555328@qq.com>
2017-06-26 14:00:19 +0800

    +上传基类库

---
+++ app/src/main/java/com/znz/compass/mimobo/ui/SpashActivity.java
@@ -0,0 +1,36 @@
+package com.znz.compass.mimobo.ui;
+
+import com.znz.compass.znzlibray.base.BaseActivity;
+
+/**
+ * Date： 2017/6/26 2017
+ * User： PSuiyi
+ * Description：
+ */
+
+public class SpashActivity extends BaseActivity {
+    @Override
+    protected int[] getLayoutResource() {
+        return new int[0];
+    }
+
+    @Override
+    protected void initializeVariate() {
+
+    }
+
+    @Override
+    protected void initializeNavigation() {
+
+    }
+
+    @Override
+    protected void initializeView() {
+
+    }
+
+    @Override
+    protected void loadDataFromServer() {
+
+    }
+}
